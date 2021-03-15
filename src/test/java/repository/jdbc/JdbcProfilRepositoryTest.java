23d1616c36938017c5f65006913f57d918f6dd6a
Thierno98 <thierno@yuxdakar.com>
2021-03-15 23:25:49 +0000

    mise à jour du prototype

--- src/test/java/repository/jdbc/JdbcProfilRepositoryTest.java
+++ src/test/java/repository/jdbc/JdbcProfilRepositoryTest.java
@@ -4,9 +4,12 @@ import domain.Profil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class JdbcProfilRepositoryTest {
 
@@ -30,6 +33,43 @@ class JdbcProfilRepositoryTest {
     }
 
     @Test
-    void getById() {
+    void getProfilsWhenExceptionOccured(){
+        System.out.println("Dans la méthode getprestation when exception occured");
+        DataSource dataSource = mock(DataSource.class);
+        when(dataSource.createConnection()).thenThrow(new RuntimeException("Base de données non disponible"));
+        jdbcBasedProfilRepository = new JdbcProfilRepository(dataSource);
+        Profil[] profils = jdbcBasedProfilRepository.getAll();
+        assertEquals(0, profils.length, "le nombre de prestations doit être 0");
+    }
+
+    @Test
+    void getByIdShouldReturnProfilWhenAvailable(){
+        Profil profil = jdbcBasedProfilRepository.getById(1);
+        assertNotNull(profil);
+        assertEquals(1, profil.getId());
+        assertEquals("", profil.getLibelle());
+    }
+
+    @Test
+    void getByIdShouldReturnPrestationWhenAvailableNotTrivial() throws SQLException {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+
+        when(dataSource.createConnection()).thenReturn(connection);
+        Statement statement = mock(Statement.class);
+        when(connection.createStatement()).thenReturn(statement);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        ResultSet resultSet2 = mock(ResultSet.class);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet2);
+        when(resultSet2.getInt(anyString())).thenReturn(5);
+        when(resultSet2.getString(anyString())).thenReturn("ok");
+
+        jdbcBasedProfilRepository = new JdbcProfilRepository(dataSource);
+
+        Profil profil = jdbcBasedProfilRepository.getById(5);
+        assertNotNull(profil);
+        assertEquals(5, profil.getId());
+        assertEquals("ok", profil.getLibelle());
     }
 }
